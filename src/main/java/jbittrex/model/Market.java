ba4fd92336b168e023d9b6e2aade74e2e43e5fa3
bbolek <burakbolek@gmail.com>
2017-08-15 07:30:19 +0300

    Getters And Setters

--- src/main/java/jbittrex/model/Market.java
+++ src/main/java/jbittrex/model/Market.java
@@ -11,4 +11,48 @@ public class Market {
 	private double minTradeSize;
 	private boolean isActive;
 	private Date created;
+	
+	public String getMarketCurrency() {
+		return marketCurrency;
+	}
+	public void setMarketCurrency(String marketCurrency) {
+		this.marketCurrency = marketCurrency;
+	}
+	public String getBaseCurrency() {
+		return baseCurrency;
+	}
+	public void setBaseCurrency(String baseCurrency) {
+		this.baseCurrency = baseCurrency;
+	}
+	public String getMarketCurrencyLong() {
+		return marketCurrencyLong;
+	}
+	public void setMarketCurrencyLong(String marketCurrencyLong) {
+		this.marketCurrencyLong = marketCurrencyLong;
+	}
+	public String getBaseCurrencyLong() {
+		return baseCurrencyLong;
+	}
+	public void setBaseCurrencyLong(String baseCurrencyLong) {
+		this.baseCurrencyLong = baseCurrencyLong;
+	}
+	public double getMinTradeSize() {
+		return minTradeSize;
+	}
+	public void setMinTradeSize(double minTradeSize) {
+		this.minTradeSize = minTradeSize;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	
 }
